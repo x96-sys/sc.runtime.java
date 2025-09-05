8187a28697087a631a27a92dbee8110b58328bad
tfs91 <tfs@disroot.org>
2025-09-05 10:51:32 -0500

    update synaptic emission

--- src/main/org/x96/sys/sc/emit/self/hive/egg/network/stimulus/impulse/nerve/synaptic/EmitSynaptic.java
+++ src/main/org/x96/sys/sc/emit/self/hive/egg/network/stimulus/impulse/nerve/synaptic/EmitSynaptic.java
@@ -3,11 +3,14 @@ package org.x96.sys.sc.emit.self.hive.egg.network.stimulus.impulse.nerve.synapti
 import org.x96.sys.sc.emit.arch.DoubleEmit;
 import org.x96.sys.sc.emit.arch.Emit;
 import org.x96.sys.sc.emit.self.hive.egg.network.stimulus.hipocampos.id.EmitId;
+import org.x96.sys.sc.emit.self.hive.egg.network.stimulus.impulse.nerve.EmitNerve;
 import org.x96.sys.sc.emit.self.hive.egg.network.stimulus.impulse.nerve.transmission.EmitTransmission;
 import org.x96.sys.sc.ir.synthetic.Neuron;
 import org.x96.sys.sc.ir.synthetic.Synaptic;
 import org.x96.sys.sc.ir.synthetic.Transmission;
 import org.x96.sys.util.S;
+
+import java.util.Arrays;
 
 public class EmitSynaptic extends DoubleEmit<Synaptic, Neuron> {
 
@@ -18,17 +21,12 @@ public class EmitSynaptic extends DoubleEmit<Synaptic, Neuron> {
 
     @Override
     public String toSC() {
+
+        String point = ".";
         String nerve = "";
-        if (p.nerve().isPresent()){
-            switch (p.nerve().get()) {
-                case Synaptic synaptic -> {
-                    nerve = "." + new EmitSynaptic(synaptic, q).toSC();
-                }
-                case Transmission transmission -> {
-                    nerve = new EmitTransmission(transmission, q).toSC();
-                }
-            }
+        if (p.nerve().isPresent()) {
+            nerve = new EmitNerve(p.nerve().get(), q).toSC();
         }
-        return String.format("%s%s", new EmitId(p.id()).toSC(), nerve);
+        return point + new EmitId(p.id()).toSC() + nerve;
     }
 }
